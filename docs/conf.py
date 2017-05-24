f91a0adf13ca02ed59050fe6b70a0b2d83c31aa8
Andrew Hicks <andrew.hicks@ga.gov.au>
2017-05-24 15:51:18 +1000

    Attempt to fix circleci sphinx 1.6.1 error

--- docs/conf.py
+++ docs/conf.py
@@ -150,6 +150,7 @@ else:
         import sphinx_rtd_theme
         html_theme = 'sphinx_rtd_theme'
         html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+        css_files = []
     except ImportError:
         html_theme = 'alabaster'
 
